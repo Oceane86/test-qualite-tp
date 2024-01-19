0c8d064be26780720d1680bccf7014a2e04fec13
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-01-19 22:43:31 +0100

    indentation : amélioration de la structure du code

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
-
 class MathOperations:
 
     def addition(a, b):
         return a + b
-    
+
     def division(a, b):
         if b == 0:
-           raise ValueError("Division par zéro impossible")
+            raise ValueError("Division par zéro impossible")
         return a / b
 
-    
-    def soustraction(a,b):
+    def soustraction(a, b):
         return a - b
-    
-    def multiplication(a,b):
+
+    def multiplication(a, b):
         return a * b
-    
+
     def power(base, exponent):
         if exponent >= 0:
             return base ** exponent
         else:
             return 1 / (base ** abs(exponent))
-        
 
-
-        
     def modulo(a, b):
         if b != 0:
             return a % b
         else:
             raise ValueError("Modulo by zero is undefined")
-        
